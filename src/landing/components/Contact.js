a5d6dd0198373aa06d0ee21a6870d7af367f15f4
AndresCastillo28 <ansa2000@hotmail.es>
2024-01-25 10:37:52 -0500

    feat: contact-information

--- src/landing/components/Contact.js
+++ src/landing/components/Contact.js
@@ -25,8 +25,7 @@ export const Contact = () => {
       setIsLoading(true);
 
       try {
-        console.log(form, 'Showing the formmmmm');
-        await portafolioApi.post("/messages", form);
+        const { data } = await portafolioApi.post("/contact-information", form);
         setForm(formContact);
         showSucessToast("The message has been sent successfully");
       } catch (error) {
@@ -36,7 +35,6 @@ export const Contact = () => {
         setErrors({});
       }
     } else {
-      // Set the 'errors' state with the validation errors
       setErrors(errors);
     }
   };
